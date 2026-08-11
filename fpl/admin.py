6e2444a848f3603e30ca6ff85b9993709db0c67d
jaypee07 <johnpaulolisa7@gmail.com>
2026-08-11 23:00:43 +0100

    Add Gameweek model with validation and admin registration

--- fpl/admin.py
+++ fpl/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# fpl/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Gameweek
+
+
+@admin.register(Gameweek)
+class GameweekAdmin(admin.ModelAdmin):
+    list_display = ("number", "is_processed", "processed_at", "updated_at")
+    list_filter = ("is_processed",)
+    ordering = ("number",)
+    readonly_fields = ("created_at", "updated_at")
--- fpl/models.py
+++ fpl/models.py
@@ -1,3 +1,42 @@
+# fpl/models.py
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Gameweek(models.Model):
+    number = models.PositiveSmallIntegerField(
+        unique=True,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(38),
+        ],
+        help_text="Official FPL gameweek number (1-38).",
+    )
+
+    is_processed = models.BooleanField(default=False)
+
+    processed_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["number"]
+        verbose_name = "Gameweek"
+        verbose_name_plural = "Gameweeks"
+
+    def clean(self):
+        if self.processed_at and not self.is_processed:
+            raise ValidationError({
+                "processed_at": "processed_at should only be set when is_processed is True."
+            })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Gameweek {self.number}"
